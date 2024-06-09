43f6b4b91e49f786d4d5d9381c73c85dfec62e25
WillyFFS <willyfrans13@gmail.com>
2024-06-09 23:33:37 +0700

    fixing

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import { Text, View, StyleSheet, Pressable, Image } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./screen/Home.js";
+import News from "./screen/News.js";
 // import “react-native-gesture-handler”;
 
 const Stack = createNativeStackNavigator();
